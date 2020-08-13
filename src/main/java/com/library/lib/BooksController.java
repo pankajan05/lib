12e761f1800120f707f8c4775a2026dd4fbd0536
pankajan05 <pankajan05@gmail.com>
2020-08-13 13:52:53 +0530

    get the parameter from url and print it in browser

--- src/main/java/com/library/lib/BooksController.java
+++ src/main/java/com/library/lib/BooksController.java
@@ -3,10 +3,15 @@ package com.library.lib;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class BooksController {
     @RequestMapping("books")
-    public String books(){
+    public String books(HttpServletRequest req){
+        HttpSession session = req.getSession();
+        session.setAttribute("cname", req.getParameter("cname"));
         return "books";
     }
 }
