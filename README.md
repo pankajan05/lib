65bc5ecf1eff2bb61667f5270e76831ad927419f
pankajan05 <pankajan05@gmail.com>
2020-08-13 11:02:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lib
+ This is for library management system that use spring boot and mysql
---
+++ src/main/java/com/library/lib/AppConfigure.java
@@ -0,0 +1,12 @@
+package com.library.lib;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppConfigure {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
