4f89c8b2ca21271e78dead2142d78a0dd27e904f
pankajan05 <pankajan05@gmail.com>
2020-08-13 14:15:48 +0530

    learn about the ModelAndView

--- src/main/java/com/library/lib/BooksController.java
+++ src/main/java/com/library/lib/BooksController.java
@@ -2,6 +2,8 @@ package com.library.lib;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -9,9 +11,10 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class BooksController {
     @RequestMapping("books")
-    public String books(HttpServletRequest req){
-        HttpSession session = req.getSession();
-        session.setAttribute("cname", req.getParameter("cname"));
-        return "books";
+    public ModelAndView books(@RequestParam("cname") String coursename){
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("cname", coursename);
+        mv.setViewName("books");
+        return mv;
     }
 }
